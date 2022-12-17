87a8c5f69a2094471433becd0dda262bee5c17a5
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-12-16 19:51:52 -0800

    added first try of binarySearch

--- JavaScriptAlgorithims.js
+++ JavaScriptAlgorithims.js
@@ -1,3 +1,24 @@
 // Binary Search Algorithim
 // Used to reduce time complexity to O(Log n)
 
+const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
+
+const binarySearch = (arr, num, left, right) => {
+    let mid = Math.floor((left + right) / 2);
+
+    if(left > right) {
+       return -1;
+    }
+    else if (num ===arr[mid]) {
+        return mid;
+    }
+    else if (num < arr[mid]) {
+        return binarySearch(arr, num, left, mid - 1);
+    }
+    else {
+        return binarySearch(arr, num, mid +1, right)
+    }
+
+};
+
+console.log(binarySearch(data, 54, 0, data.length - 1));
