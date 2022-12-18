3e0b5c83e6da0ec25a08326348a27e1313558023
Diego Rivera <93798677+Diegorivera1110@users.noreply.github.com>
2022-12-17 21:29:12 -0800

    added comments to first solution for linear search

--- BinarySearch(JavaScript).js
+++ BinarySearch(JavaScript).js
@@ -1,11 +1,14 @@
 // Binary Search Algorithim
 // Used to reduce time complexity to O(Log n)
 
+
+// This first solution is a Linear Search Approach with the time complexity being O(n).
 const data = [12, 23, 38, 40, 54, 62, 71, 87, 99];
 
 const binarySearch = (arr, num, left, right) => {
     let mid = Math.floor((left + right) / 2);
 
+    // range overalapped, so number doesn't exist in data array
     if(left > right) {
        return -1;
     }
@@ -13,12 +16,14 @@ const binarySearch = (arr, num, left, right) => {
         return mid;
     }
     else if (num < arr[mid]) {
+        // call again with a new right value
         return binarySearch(arr, num, left, mid - 1);
     }
     else {
+        // call again with a new left value
         return binarySearch(arr, num, mid +1, right)
     }
-
 };
 
-console.log(binarySearch(data, 54, 0, data.length - 1));
+// set initial left and right values on first call 
+// console.log(binarySearch(data, 87, 0, data.length - 1));
---
+++ README.md
@@ -0,0 +1,3 @@
+# The reason I created this Repo
+
+- I created this repository to keep track of common and specific algorithm solutions I solved. 
